f10a8461b8889beec6d3799ea907add7169d1543
James Kageni <james.kageni@gmail.com>
2018-09-19 15:29:43 +0300

    added the new event page

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField
+from flask_wtf.file import FileField, FileRequired
 from wtforms.validators import Required
 
 
@@ -11,4 +12,13 @@ class PostForm(FlaskForm):
 
 class ThreadForm(FlaskForm):
     comment = TextAreaField('Say something about this', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+class EventsForm(FlaskForm):
+    event_title = TextAreaField('Event title')
+    event_desc = TextAreaField('Description')
+    event_loc = TextAreaField('Location')
+    event_charges = TextAreaField('Charges')
+    pic = FileField(validators=[FileRequired()])
     submit = SubmitField('Submit')
